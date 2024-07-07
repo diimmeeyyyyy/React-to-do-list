cf6381d4baa183450071788344fc9590ac4d5523
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-07-07 14:09:12 +0200

    added useState to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import "./style.css";
 
 export default function App() {
-  return <form className="new-item-form">
-    
-  </form>;
+  const [newItem, setNewItem] = useState("");
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="Item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="Item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To-do List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
